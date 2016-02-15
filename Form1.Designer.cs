690a73625818b825c3943971f56e42f895feb08d
Alex <digwnews@gmail.com>
2016-02-15 21:43:42 +0300

    table build

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ExpertSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoScroll = true;
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 0F));
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(772, 226);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(372, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.AutoScroll = true;
+            this.splitContainer1.Panel1.Controls.Add(this.tableLayoutPanel1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Size = new System.Drawing.Size(772, 452);
+            this.splitContainer1.SplitterDistance = 226;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 452);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace ExpertSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+            typeof(TableLayoutPanel)
+           .GetProperty("DoubleBuffered",
+              System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
+           .SetValue(tableLayoutPanel1, true, null);
+            buildTable(15);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private int AddTableRow(List<Control> objs, int n)
+        {
+            int rowIndex = this.tableLayoutPanel1.RowCount++;
+            
+            //int percent = 100 / (n + 1);
+         
+            for (int i = 0; i < objs.Count; i++)
+            {
+                tableLayoutPanel1.Controls.Add(objs[i], i, rowIndex);
+            }
+
+            
+            return rowIndex;
+        }
+
+        private void addColumns(int count)
+        {
+            //TODO: percents
+            tableLayoutPanel1.ColumnCount = count;
+        }
+
+
+        /*
+         * n - size of matrix
+         * 
+         */
+        private void buildTable(int n)
+        {
+            tableLayoutPanel1.Visible = false;
+            RowStyle style = new RowStyle(SizeType.AutoSize);
+            tableLayoutPanel1.RowStyles.Add(style);
+            //tableLayoutPanel1.VerticalScroll.Maximum = 200;
+            const int NUMBER_OF_ADDITIONAL_COLS = 3;
+            addColumns(n + NUMBER_OF_ADDITIONAL_COLS);
+
+            // build head
+            Label localCriteria = new Label();
+            localCriteria.Text = "Локальный критерий";
+
+            Label[] criterias = new Label[n];
+            for (int i = 0; i < criterias.Length; i++)
+            {
+                criterias[i] = new Label();
+                SetDoubleBuffered(criterias[i]);
+                criterias[i].Text = "K" + (i + 1);
+            }
+
+            Label coef = new Label();
+            coef.Text = "C";
+
+            Label alpha = new Label();
+            alpha.Text = "alpha";
+
+            List<Control> head = new List<Control>();
+            head.Add(localCriteria);
+            head.AddRange(criterias);
+            head.Add(coef);
+            head.Add(alpha);
+
+            AddTableRow(head, n);
+
+            // build input rows:
+
+            for(int i = 0; i < n; i++) 
+            {
+                List<Control> row = new List<Control>();
+                Label name = new Label();
+                name.Text = "K" + (i + 1);
+                row.Add(name);
+
+                for (int j = 0; j < n; j++)
+                {
+                    TextBox txtbox = new TextBox();
+                    SetDoubleBuffered(txtbox);
+                    row.Add(txtbox);
+                }
+
+                TextBox coefTextBox = new TextBox();
+                coefTextBox.Enabled = false;
+                row.Add(coefTextBox);
+                TextBox alphaTextBox = new TextBox();
+                alphaTextBox.Enabled = false;
+                row.Add(alphaTextBox);
+
+                AddTableRow(row, n);
+            }
+
+            tableLayoutPanel1.Visible = true;
+        }
+
+        public static void SetDoubleBuffered(Control control)
+        {
+            // set instance non-public property with name "DoubleBuffered" to true
+            typeof(Control).InvokeMember("DoubleBuffered",
+                BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic,
+                null, control, new object[] { true });
+        }
+    }
+}
